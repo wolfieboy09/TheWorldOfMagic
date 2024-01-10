4184fd3352a5136e1c6b19204fc9e7c167f03bee
Wolfieboy09 <wolfieboy091yt@gmail.com>
2024-01-10 11:38:19 -0500

    Added tooltip

--- src/main/java/net/wolfieboy09/theworldofmagic/item/custom/OreDetectorItem.java
+++ src/main/java/net/wolfieboy09/theworldofmagic/item/custom/OreDetectorItem.java
@@ -6,13 +6,18 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.wolfieboy09.theworldofmagic.util.ModTags;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 public class OreDetectorItem extends Item {
@@ -51,12 +56,18 @@ public class OreDetectorItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
-    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel,@NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.theworldofmagic.ore_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+    private void outputValuableCoordinates(@NotNull BlockPos blockPos, @NotNull Player player, @NotNull Block block) {
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"));
     }
 
-    private boolean isValuableBlock(BlockState state) {
+    private boolean isValuableBlock(@NotNull BlockState state) {
         return state.is(ModTags.Blocks.ORE_DETECTOR_VALUABLES);
     }
 }
